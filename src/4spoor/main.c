cf6b22a3c192ad8e3017bf6b84f52b62a9826841
Arthur Bennis <sinneb@gmail.com>
2016-10-11 23:06:43 +0200

    code cleanup

--- src/4spoor/main.c
+++ src/4spoor/main.c
@@ -47,26 +47,16 @@ const uint32_t LCDColorarray[] = { LCD_COLOR_YELLOW, LCD_COLOR_GREEN, LCD_COLOR_
 #define AUDIO_DMA_BUFFER_SIZE4 (AUDIO_DMA_BUFFER_SIZE >> 2)
 #define AUDIO_DMA_BUFFER_SIZE8 (AUDIO_DMA_BUFFER_SIZE >> 3)
 
-char audioFilename1[] = "0:sound.wav";
-char audioFilename2[] = "0:sound2.wav";
-char audioFilename3[] = "0:sound3.wav";
-char audioFilename4[] = "0:sound4.wav";
-
 char *audioFilename[4] = {"0:sound.wav", "0:sound2.wav", "0:sound3.wav", "0:sound4.wav"};
-//char audioFilename[4][50];
-//audioFilename[][0] = "0:sound.wav";
-//char audioFilenames[5][100];
-//audioFilenames[0] = "0:sound.wav";
 
+// audio buffers
 static uint8_t int_bufProcessedOut[AUDIO_DMA_BUFFER_SIZE];
 static uint8_t audioOutBuf[AUDIO_DMA_BUFFER_SIZE];
-
-static uint8_t audioBufFile1[AUDIO_DMA_BUFFER_SIZE];
-static uint8_t audioBufFile2[AUDIO_DMA_BUFFER_SIZE];
-static uint8_t audioBufFile3[AUDIO_DMA_BUFFER_SIZE];
-static uint8_t audioBufFile4[AUDIO_DMA_BUFFER_SIZE];
-
-uint8_t *audioBufFiles[4][AUDIO_DMA_BUFFER_SIZE];
+static uint8_t audioBufferFile[4][AUDIO_DMA_BUFFER_SIZE];
+float f_bufPre_left[4][1024];
+float f_bufPost_left[4][1024];
+float f_bufPre_right[4][1024];
+float f_bufPost_right[4][1024];
 
 uint16_t taptempo[4];
 uint8_t taptempocounter;
@@ -74,26 +64,6 @@ uint32_t taptempo_prev = 0;
 float global_tempo = 100;
 
 uint32_t prevTick = 0;
-
-float f_bufPre_left[1024];
-float f_bufPre_right[1024];
-float f_bufPost_left[1024];
-float f_bufPost_right[1024];
-
-float f_bufPre2_left[1024];
-float f_bufPre2_right[1024];
-float f_bufPost2_left[1024];
-float f_bufPost2_right[1024];
-
-float f_bufPre3_left[1024];
-float f_bufPre3_right[1024];
-float f_bufPost3_left[1024];
-float f_bufPost3_right[1024];
-
-float f_bufPre4_left[1024];
-float f_bufPre4_right[1024];
-float f_bufPost4_left[1024];
-float f_bufPost4_right[1024];
 
 float f_bufPost_mixdown_left[1024];
 float f_bufPost_mixdown_right[1024];
@@ -106,7 +76,7 @@ extern USBH_HandleTypeDef hUSBH;
 extern SAI_HandleTypeDef haudio_out_sai;
 
 FIL audio_file[4];
-UINT bytes_read;
+UINT bytes_read[4];
 UINT bytes_read2;
 UINT bytes_read3;
 UINT bytes_read4;
@@ -169,47 +139,24 @@ int main() {
 }
 
 static void init_after_USB() {
-    if (f_open(&audio_file[0], audioFilename[0], FA_READ) == FR_OK) {
+  // open samples
+  for (int j=0; j < 4; j++) {
+    if (f_open(&audio_file[j], audioFilename[j], FA_READ) == FR_OK) {
 		// f_open ok
-      f_lseek(&audio_file[0], f_size(&audio_file[0]));
+      f_lseek(&audio_file[j], f_size(&audio_file[j]));
     } else {
         BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize() / 2 - 8,
-                                (uint8_t *)"ERROR", CENTER_MODE);
+                                (uint8_t *)"Load error", CENTER_MODE);
       Error_Handler();
     }
-	
-    if (f_open(&audio_file[1], audioFilename[1], FA_READ) == FR_OK) {
-		// f_open ok
-      f_lseek(&audio_file[1], f_size(&audio_file[1]));
-    } else {
-        BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize() / 2 - 8,
-                                (uint8_t *)"ERROR2", CENTER_MODE);
-      Error_Handler();
-    }
-
-        if (f_open(&audio_file[2], audioFilename[2], FA_READ) == FR_OK) {
-    // f_open ok
-          f_lseek(&audio_file[2], f_size(&audio_file[2]));
-        } else {
-            BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize() / 2 - 8,
-                                    (uint8_t *)"ERROR3", CENTER_MODE);
-          Error_Handler();
-        }
-        if (f_open(&audio_file[3], audioFilename[3], FA_READ) == FR_OK) {
-    // f_open ok
-          f_lseek(&audio_file[3], f_size(&audio_file[3]));
-        } else {
-            BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize() / 2 - 8,
-                                    (uint8_t *)"ERROR4", CENTER_MODE);
-          Error_Handler();
-        }
+  }
 	
 	float bpm = getBPM(&audio_file[1]);
 	char a[] = "";
 	snprintf(a, 6, "%f",bpm);
     BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize() / 2 - 8, (uint8_t *)a, CENTER_MODE);
   
-    initAudio();
+  initAudio();
 }
 
 float getBPM(FIL *audio_file) {
@@ -253,25 +200,25 @@ void BSP_AUDIO_OUT_TransferComplete_CallBack(void) {
 	memcpy(&audioOutBuf[AUDIO_DMA_BUFFER_SIZE2], int_bufProcessedOut, AUDIO_DMA_BUFFER_SIZE2);
 	
 	
-	if(bytes_read<2048) {
+	if(bytes_read[0]<2048) {
 		//memset(audioOutBuf, 0, sizeof audioOutBuf);
-		memset(audioBufFile1, 0, sizeof audioBufFile1);
-		//memset(audioBufFile2, 0, sizeof audioBufFile2);
+		memset(audioBufferFile[0], 0, sizeof audioBufferFile[0]);
+		//memset(audioBufferFile[1], 0, sizeof audioBufferFile[1]);
 	}
-	if(bytes_read2<2048) {
+	if(bytes_read[1]<2048) {
 		//memset(audioOutBuf, 0, sizeof audioOutBuf);
-		//memset(audioBufFile1, 0, sizeof audioBufFile1);
-		memset(audioBufFile2, 0, sizeof audioBufFile2);
+		//memset(audioBufferFile[0], 0, sizeof audioBufferFile[0]);
+		memset(audioBufferFile[1], 0, sizeof audioBufferFile[1]);
 	}
-	if(bytes_read3<2048) {
+	if(bytes_read[2]<2048) {
 		//memset(audioOutBuf, 0, sizeof audioOutBuf);
-		memset(audioBufFile3, 0, sizeof audioBufFile3);
-		//memset(audioBufFile2, 0, sizeof audioBufFile2);
+		memset(audioBufferFile[2], 0, sizeof audioBufferFile[2]);
+		//memset(audioBufferFile[1], 0, sizeof audioBufferFile[1]);
 	}
-	if(bytes_read4<2048) {
+	if(bytes_read[3]<2048) {
 		//memset(audioOutBuf, 0, sizeof audioOutBuf);
-		//memset(audioBufFile1, 0, sizeof audioBufFile1);
-		memset(audioBufFile4, 0, sizeof audioBufFile4);
+		//memset(audioBufferFile[0], 0, sizeof audioBufferFile[0]);
+		memset(audioBufferFile[3], 0, sizeof audioBufferFile[3]);
 	}
 }
 
@@ -282,73 +229,48 @@ void computeAudio() {
 	short bufsize = (short)(2048 * rate);
 	// garantue a multiple of 4
 	bufsize -= bufsize%4;
-	
-  // clear global buffers
-  memset(audioBufFile1, 0, sizeof audioBufFile1);
-  memset(audioBufFile2, 0, sizeof audioBufFile2);
-  memset(audioBufFile3, 0, sizeof audioBufFile3);
-  memset(audioBufFile4, 0, sizeof audioBufFile4);
   
-	// read chunks from USB
-	f_read(&audio_file[0], audioBufFile1, bufsize, &bytes_read);
-	f_read(&audio_file[1], audioBufFile2, bufsize, &bytes_read2);
-	f_read(&audio_file[2], audioBufFile3, bufsize, &bytes_read3);
-	f_read(&audio_file[3], audioBufFile4, bufsize, &bytes_read4);
+  // for each sample
+  for (int i=0; i < 4; i++) {
 	
-	// 2's-complement signed integers -> short (-32k -> +32k) -> float (-1 -> +1)
-	for (int j=0; j < bufsize; j=j+4) {
+    // clear global buffers
+    memset(audioBufferFile[i], 0, sizeof audioBufferFile[i]);
+  
+  	// read chunks from USB
+  	f_read(&audio_file[i], audioBufferFile[i], bufsize, &bytes_read[i]);
+	
+  	// 2's-complement signed integers -> short (-32k -> +32k) -> float (-1 -> +1)
+  	for (int j=0; j < bufsize; j=j+4) {
 		
-		// to short
-		short lefty = (short) (audioBufFile1[j+1]<<8 | (audioBufFile1[j] & 0xFF));
-		short righty = (short) (audioBufFile1[j+3]<<8 | (audioBufFile1[j+2] & 0xFF));
+  		// to short
+  		short lefty = (short)((audioBufferFile[i][j+1])<<8 | ((audioBufferFile[i][j]) & 0xFF));
+  		short righty = (short)((audioBufferFile[i][j+3])<<8 | ((audioBufferFile[i][j+2]) & 0xFF));
 		
-		short lefty2 = (short) (audioBufFile2[j+1]<<8 | (audioBufFile2[j] & 0xFF));
-		short righty2 = (short) (audioBufFile2[j+3]<<8 | (audioBufFile2[j+2] & 0xFF));
-    
-		short lefty3 = (short) (audioBufFile3[j+1]<<8 | (audioBufFile3[j] & 0xFF));
-		short righty3 = (short) (audioBufFile3[j+3]<<8 | (audioBufFile3[j+2] & 0xFF));
-    
-		short lefty4 = (short) (audioBufFile4[j+1]<<8 | (audioBufFile4[j] & 0xFF));
-		short righty4 = (short) (audioBufFile4[j+3]<<8 | (audioBufFile4[j+2] & 0xFF));
-		//shortleft[j/4] = lefty;
-		//shortright[j/4] = righty;
-		
-		// to float
-		f_bufPre_left[j/4] = ((float)lefty/32768);
-		f_bufPre_right[j/4] = ((float)righty/32768);
-		
-		f_bufPre2_left[j/4] = ((float)lefty2/32768);
-		f_bufPre2_right[j/4] = ((float)righty2/32768);
-    
-		f_bufPre3_left[j/4] = ((float)lefty3/32768);
-		f_bufPre3_right[j/4] = ((float)righty3/32768);
-    
-		f_bufPre4_left[j/4] = ((float)lefty4/32768);
-		f_bufPre4_right[j/4] = ((float)righty4/32768);
-}
+  		// to float
+  		f_bufPre_left[i][j/4] = ((float)lefty/32768);
+  		f_bufPre_right[i][j/4] = ((float)righty/32768);
+    }
+  
+    // interpolation
+  	inter1parray( f_bufPre_left[i], bufsize / 4, f_bufPost_left[i], 512 );
+  	inter1parray( f_bufPre_right[i], bufsize / 4, f_bufPost_right[i], 512 );
+  }
 
-	inter1parray( f_bufPre_left, bufsize / 4, f_bufPost_left, 512 );
-	inter1parray( f_bufPre_right, bufsize / 4, f_bufPost_right, 512 );
-	inter1parray( f_bufPre2_left, bufsize / 4, f_bufPost2_left, 512 );
-	inter1parray( f_bufPre2_right, bufsize / 4, f_bufPost2_right, 512 );
-	inter1parray( f_bufPre3_left, bufsize / 4, f_bufPost3_left, 512 );
-	inter1parray( f_bufPre3_right, bufsize / 4, f_bufPost3_right, 512 );
-	inter1parray( f_bufPre4_left, bufsize / 4, f_bufPost4_left, 512 );
-	inter1parray( f_bufPre4_right, bufsize / 4, f_bufPost4_right, 512 );
+
 	
 	// the mix
 	// float to short
 	for (int k=0; k<2048; k=k+4) {
     
     // mix
-    f_bufPost_mixdown_left[k/4] = (0.8 * f_bufPost_left[k/4])
-                          + (0.8 * f_bufPost2_left[k/4])
-                          + (0.8 * f_bufPost3_left[k/4])
-                          + (0.8 * f_bufPost4_left[k/4]);
-    f_bufPost_mixdown_right[k/4] = (0.8 * f_bufPost_right[k/4])
-                          + (0.8 * f_bufPost2_right[k/4])
-                          + (0.8 * f_bufPost3_right[k/4])
-                          + (0.8 * f_bufPost4_right[k/4]);
+    f_bufPost_mixdown_left[k/4] = (0.8 * f_bufPost_left[0][k/4])
+                          + (0.8 * f_bufPost_left[1][k/4])
+                          + (0.8 * f_bufPost_left[2][k/4])
+                          + (0.8 * f_bufPost_left[3][k/4]);
+    f_bufPost_mixdown_right[k/4] = (0.8 * f_bufPost_right[0][k/4])
+                          + (0.8 * f_bufPost_right[1][k/4])
+                          + (0.8 * f_bufPost_right[2][k/4])
+                          + (0.8 * f_bufPost_right[3][k/4]);
     
 		// to short
 		resleft[k/4] = (short)(f_bufPost_mixdown_left[k/4] * 32768);
@@ -360,74 +282,6 @@ void computeAudio() {
 		int_bufProcessedOut[k+2] = resright[k/4]&0xff;
 		
 	}
-}
-
-float elephant (float x, float y[])
-{
-	float z = x - 1/2.0;
-	float even1 = y[1]+y[0], odd1 = y[1]-y[0];
-	float even2 = y[2]+y[-1], odd2 = y[2]-y[-1];
-	float c0 = even1*0.45868970870461956 + even2*0.04131401926395584;
-	float c1 = odd1*0.48068024766578432 + odd2*0.17577925564495955;
-	float c2 = even1*-0.246185007019907091 + even2*0.24614027139700284;
-	float c3 = odd1*-0.36030925263849456 + odd2*0.10174985775982505;
-	return ((c3*z+c2)*z+c1)*z+c0;
-}
-
-void interp5( float a[], int n, float b[], int m )
-{
-    float step = (float)( (float)n - 1 ) / ((float)m - 1);
-    for( int j = 0; j < m; j ++ ){
-        b[j] = elephant( j*step, a);
-    }
-}
-
-float interp1( float x, float a[], int n )
-{
-    if( x <= 0 )  return a[0];
-    if( x >= n - 1 )  return a[n-1];
-    int j = (int)x;
-    return a[j] + (x - j) * (a[j+1] - a[j]);
-}
-
-    // linear interpolate array a[] -> array b[]
-void inter1parray( float a[], int n, float b[], int m )
-{
-    float step = (float)( (float)n - 1 ) / ((float)m - 1);
-    for( int j = 0; j < m; j ++ ){
-        b[j] = interp1( j*step, a, n );
-    }
-}
-
-//..............................................................................
-    // parabola through 3 points, -1 < x < 1
-
-float parabola( float x, float f_1, float f0, float f1 )
-{
-    if( x <= -1 )  return f_1; 
-    if( x >= 1 )  return f1; 
-    float l = f0 - x * (f_1 - f0);
-    float r = f0 + x * (f1 - f0);
-    return (l + r + x * (r - l)) / 2;
-}
-
-    // quadratic interpolate x in an array
-float interp2( float x, float a[], int n )
-{
-    if( x <= .5  ||  x >= n - 1.5 )
-        return interp1( x, a, n );
-    int j = (int)( x + .5 );
-    float t = 2 * (x - j);  // -1 .. 1
-    return parabola( t, (a[j-1] + a[j]) / 2, a[j], (a[j] + a[j+1]) / 2 );
-}
-
-    // quadratic interpolate array a[] -> array b[]
-void interp2array( float a[], int n, float b[], int m )
-{
-    float step = (float)( n - 1 ) / (m - 1);
-    for( int j = 0; j < m; j ++ ){
-        b[j] = interp2( j*step, a, n );
-    }
 }
 
 void OTG_FS_IRQHandler(void) {
@@ -445,48 +299,24 @@ void EXTI15_10_IRQHandler(void) {
   }
 }
 
+void drawButton(uint16_t x, uint16_t y, uint8_t * textstring) {
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_FillRect(x,y,110,100);
+  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
+  BSP_LCD_FillRect(x+5,y+5,100,90);
+	BSP_LCD_SetFont(&Font24);
+	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+  BSP_LCD_SetBackColor(LCD_COLOR_ORANGE);
+	BSP_LCD_DisplayStringAt(x+45, y+40, textstring, LEFT_MODE);
+}
+
 void drawInterface() {
   BSP_LCD_Clear(LCD_COLOR_BLUE);
-	
-	//BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-	//BSP_LCD_DrawVLine(160,0,320);
     
-  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-  BSP_LCD_FillRect(5,5,110,100);
-  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-  BSP_LCD_FillRect(10,10,100,90);
-	BSP_LCD_SetFont(&Font24);
-	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-  BSP_LCD_SetBackColor(LCD_COLOR_ORANGE);
-	BSP_LCD_DisplayStringAt(50, 45, (uint8_t *)"1", LEFT_MODE);
-  
-  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-  BSP_LCD_FillRect(120,5,115,100);
-  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-  BSP_LCD_FillRect(125,10,105,90);
-	BSP_LCD_SetFont(&Font24);
-	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-  BSP_LCD_SetBackColor(LCD_COLOR_ORANGE);
-	BSP_LCD_DisplayStringAt(170, 45, (uint8_t *)"2", LEFT_MODE);
-  
-  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-  BSP_LCD_FillRect(240,5,115,100);
-  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-  BSP_LCD_FillRect(245,10,105,90);
-	BSP_LCD_SetFont(&Font24);
-	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-  BSP_LCD_SetBackColor(LCD_COLOR_ORANGE);
-	BSP_LCD_DisplayStringAt(290, 45, (uint8_t *)"3", LEFT_MODE);
-
-  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-  BSP_LCD_FillRect(360,5,115,100);
-  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-  BSP_LCD_FillRect(365,10,105,90);
-	BSP_LCD_SetFont(&Font24);
-	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-  BSP_LCD_SetBackColor(LCD_COLOR_ORANGE);
-	BSP_LCD_DisplayStringAt(410, 45, (uint8_t *)"4", LEFT_MODE);
-
+  drawButton(10,5,(uint8_t *)"1");
+  drawButton(125,5,(uint8_t *)"2");
+  drawButton(240,5,(uint8_t *)"3");
+  drawButton(355,5,(uint8_t *)"4");
 }
 
 // void HAL_GPIO_EXTI_IRQHandler points to this:
