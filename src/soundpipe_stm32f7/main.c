f7729f2cd1dc8f8ecf702e5542882503805c404c
Arthur Bennis <sinneb@gmail.com>
2016-11-23 20:32:51 +0100

    4 ADC readouts over DMA

--- src/soundpipe_stm32f7/main.c
+++ src/soundpipe_stm32f7/main.c
@@ -24,6 +24,7 @@ soundpipe on stm32f7 discovery
 
 static sp_data *sp;
 static sp_blsaw *blsaw;
+static sp_blsaw *blsaw2;
 
 #define VOLUME 90
 #define SAMPLE_RATE 48000
@@ -40,11 +41,23 @@ extern SAI_HandleTypeDef haudio_out_sai;
 void initAudio();
 void computeAudio();
 void drawInterface();
+void ConfigureADC();
+void ConfigureDMA();
+
+uint32_t g_ADCValue;
+int g_MeasurementNumber;
+ADC_HandleTypeDef g_AdcHandle;
+DMA_HandleTypeDef  g_DmaHandle;
+enum{ ADC_BUFFER_LENGTH = 8192 };
+uint32_t g_ADCBuffer[ADC_BUFFER_LENGTH];
 
 int main() {
   CPU_CACHE_Enable();
   HAL_Init();
   SystemClock_Config(); 
+  ConfigureADC();
+  ConfigureDMA();
+      HAL_ADC_Start_DMA(&g_AdcHandle, g_ADCBuffer, ADC_BUFFER_LENGTH);
   
   BSP_LED_Init(LED_GREEN);
   BSP_LED_Off(LED_GREEN);
@@ -64,18 +77,164 @@ int main() {
   sp->sr = 48000;
 
   sp_blsaw_create(&blsaw);
+  sp_blsaw_create(&blsaw2);
 
   sp_blsaw_init(sp, blsaw);
   *blsaw->freq = 440;
   *blsaw->amp = 0.5f;
+  
+  sp_blsaw_init(sp, blsaw2);
+  *blsaw2->freq = 400;
+  *blsaw2->amp = 0.5f;
+  
 
   while (1) {		
-	  //nothing is done here	
+    //if (HAL_ADC_PollForConversion(&g_AdcHandle, 1000000) == HAL_OK)
+    //        {
+        //        g_ADCValue = HAL_ADC_GetValue(&g_AdcHandle);
+      //          g_MeasurementNumber++;
+            		char a[] = "";
+            		sprintf(a, "%ld", g_ADCValue);
+                BSP_LCD_DisplayStringAt(50, 50, (uint8_t *)a, LEFT_MODE);
+    //        }
   }
 
   return 0;
 }
 
+
+void ConfigureADC()
+{
+    GPIO_InitTypeDef gpioInit;
+ 
+    // MCU pin for A1 = PF10
+    // Function for PF10 = ADC3_IN8
+    // 5v compatible port
+    __GPIOF_CLK_ENABLE();
+    __GPIOA_CLK_ENABLE();
+    __ADC3_CLK_ENABLE();
+ 
+    gpioInit.Pin = GPIO_PIN_10|GPIO_PIN_9|GPIO_PIN_8;
+    gpioInit.Mode = GPIO_MODE_ANALOG;
+    gpioInit.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOF, &gpioInit);
+
+    gpioInit.Pin = GPIO_PIN_0;
+    gpioInit.Mode = GPIO_MODE_ANALOG;
+    gpioInit.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &gpioInit);
+ 
+    HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(ADC_IRQn);
+ 
+    ADC_ChannelConfTypeDef adcChannel;
+ 
+    g_AdcHandle.Instance = ADC3;
+ 
+    g_AdcHandle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+    g_AdcHandle.Init.Resolution = ADC_RESOLUTION_12B;
+    g_AdcHandle.Init.ScanConvMode = ENABLE;
+    g_AdcHandle.Init.ContinuousConvMode = ENABLE;
+    g_AdcHandle.Init.DiscontinuousConvMode = DISABLE;
+    g_AdcHandle.Init.NbrOfDiscConversion = 0;
+    g_AdcHandle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    g_AdcHandle.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+    g_AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    g_AdcHandle.Init.NbrOfConversion = 4;
+    g_AdcHandle.Init.DMAContinuousRequests = ENABLE;
+    g_AdcHandle.Init.EOCSelection = DISABLE;
+ 
+    HAL_ADC_Init(&g_AdcHandle);
+ 
+    // adcChannel.Channel = ADC_CHANNEL_8;
+    // adcChannel.Rank = 1;
+    // adcChannel.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+    // adcChannel.Offset = 0;
+    //
+    // if (HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel) != HAL_OK)
+    // {
+    //     //asm("bkpt 255");
+    //   Error_Handler();
+    // }
+    
+    adcChannel.Channel = ADC_CHANNEL_0;
+    adcChannel.Rank = 1;
+    adcChannel.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+    if (HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+      /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+      */
+    adcChannel.Channel = ADC_CHANNEL_8;
+    adcChannel.Rank = 2;
+    if (HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+      /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+      */
+    adcChannel.Channel = ADC_CHANNEL_7;
+    adcChannel.Rank = 3;
+    if (HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+      /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+      */
+    adcChannel.Channel = ADC_CHANNEL_6;
+    adcChannel.Rank = 4;
+    if (HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel) != HAL_OK)
+    {
+      Error_Handler();
+    }
+    
+}
+
+void ConfigureDMA()
+{
+    __DMA2_CLK_ENABLE(); 
+    g_DmaHandle.Instance = DMA2_Stream1;
+  
+    g_DmaHandle.Init.Channel  = DMA_CHANNEL_2;
+    g_DmaHandle.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    g_DmaHandle.Init.PeriphInc = DMA_PINC_DISABLE;
+    g_DmaHandle.Init.MemInc = DMA_MINC_ENABLE;
+    g_DmaHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+    g_DmaHandle.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+    g_DmaHandle.Init.Mode = DMA_CIRCULAR;
+    g_DmaHandle.Init.Priority = DMA_PRIORITY_HIGH;
+    g_DmaHandle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;         
+    g_DmaHandle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_HALFFULL;
+    g_DmaHandle.Init.MemBurst = DMA_MBURST_SINGLE;
+    g_DmaHandle.Init.PeriphBurst = DMA_PBURST_SINGLE; 
+    
+    HAL_DMA_Init(&g_DmaHandle);
+    
+    __HAL_LINKDMA(&g_AdcHandle, DMA_Handle, g_DmaHandle);
+
+    HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);   
+    HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
+    {
+      g_ADCValue = g_ADCBuffer[2004];//std::accumulate(g_ADCBuffer, g_ADCBuffer + ADC_BUFFER_LENGTH, 0) / ADC_BUFFER_LENGTH;
+        g_MeasurementNumber += ADC_BUFFER_LENGTH;
+    }
+ 
+void DMA2_Stream1_IRQHandler()
+{
+    HAL_DMA_IRQHandler(&g_DmaHandle);
+}
+
+void ADC_IRQHandler()
+{
+    HAL_ADC_IRQHandler(&g_AdcHandle);
+}
 
 void initAudio() {
     if (BSP_AUDIO_OUT_Init(OUTPUT_DEVICE_HEADPHONE, VOLUME, SAMPLE_RATE) != 0) {
@@ -109,11 +268,15 @@ void computeAudio() {
   // compute 2048 samples -> 512 audiosamples
   for(int i = 0; i < 1024; i+=2) {
     SPFLOAT tmp = 0;
+    SPFLOAT tmp2 = 0;
     sp_blsaw_compute(sp, blsaw, NULL, &tmp);
+    sp_blsaw_compute(sp, blsaw2, NULL, &tmp2);
+    
+    SPFLOAT mixOut = (0.5f * tmp + 0.5f * tmp2);
 
     // channel outputs in 2's comp / signed int
-    int_bufProcessedOut[i] = (tmp * 32767);
-    int_bufProcessedOut[i+1] = (tmp * 32767);
+    int_bufProcessedOut[i] = (mixOut * 32767);
+    int_bufProcessedOut[i+1] = (mixOut * 32767);
   }
 }
 
